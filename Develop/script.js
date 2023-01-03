003b68d3f50ca36f6ea775c64d93b88ef7205066
mushroom <shaelah.mai@gmail.com>
2023-01-03 21:05:49 +1000

    partially completed generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,65 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+document.querySelector("#generate").addEventListener("click", writePassword);
 
-  passwordText.value = password;
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
-}
+// Pop-up windows fro user input 
+var generatePassword = function() {
+  var confirmLength = window.prompt("How long do you wish your password to be? It can be between 8 and 128 characters.");
+    if (confirmLength <= 8 || confirmLength >=128) {
+      window.alert("Password must be between 8 and 128 characters.")
+      var confirmLength = window.prompt("How long do you wish your password to be? It can be between 8 and 128 characters.");
+    }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  var confirmLowercase = window.confirm("Click OK if you wish to include lowercase letters?");
+  var confirmUppercase = window.confirm("Click OK if you wish to inlcude uppercase letters?");
+  var confirmNumber = window.confirm("Click OK if you wish to inlcude numbers?");
+  var confirmSpecialCharacters = window.confirm("Click OK if you wish to include special characters?");
+    
+  if(confirmUppercase === false && confirmLowercase === false && confirmNumber === false && confirmSpecialCharacters === false) {
+      window.alert("You must choose at least on character type, please try again.")
+      var confirmLowercase = window.confirm("Click OK if you wish to include lowercase letters?");
+      var confirmUppercase = window.confirm("Click OK if you wish to inlcude uppercase letters?");
+      var confirmNumber = window.confirm("Click OK if you wish to inlcude numbers?");
+      var confirmSpecialCharacters = window.confirm("Click OK if you wish to include special characters?");
+    }
+  }
+
+  // Combine arrays based on choices made above
+  var passwordCharacters = {
+    if (confirmLowercase === true) {
+      password = password.concat(lowercase);
+    }
+
+    if (confirmUppercase === true) {
+      password = password.concat(uppercase);
+    }
+
+    if (confirmNumber === true) {
+      password = password.concat(number);
+    }
+
+    if (confirmSpecialCharacters === true) {
+      password = password.concat(specialCharacters);
+    }
+  }
+
+  // Create a random generator for character types selected and chose number of characters required
+  for (var i = 0; i < confirmLength; i++) {
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * confirmLength)];
+  }
+
+  // Display password on screen 
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password")
+    
+    passwordText.value = password;
+  }
+
+  
